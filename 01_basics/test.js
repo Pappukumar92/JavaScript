f9f423eed8da0e16b1a1049684ca2421340c4c25
Pappukumar92 <161245494+Pappukumar92@users.noreply.github.com>
2025-04-25 20:00:44 +0000

    container config done

--- 01_basics/test.js
+++ 01_basics/test.js
@@ -1,1 +1,16 @@
-console.log("Pappu Yadav")
+const accountId = 144453
+let accountEmail ="papuyav46@gmial.com"
+var accountPassword = "12334"
+accountCity ="jaipur"
+
+// we can declare variable with the help of let or var 
+
+// accountId = 2  not variable
+
+accountEmail ="haf@.com"
+accountPassword ="123455"
+accountCity ="Bangal"
+
+console.log(accountId);
+
+console.table([accountId,accountEmail,accountPassword,accountCity])
